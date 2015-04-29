3f9538c263f44aff734cbb5a788502e73142c711
pppgod <strangeyun@gmail.com>
2015-04-30 03:14:09 +0900

    아이템 선택시 버튼 내용 변경
    컨텍스트 메뉴의 그룹아이디 부여를 위해 자바 파일내에서 메뉴 생성
    
    그로 인해 context_order.xml파일이 필요가 없어졌으나 나중을 대비하여 일단은 남겨두었습니다.

--- PsRankingClient/app/src/main/java/com/project/ctp/psrankingclient/SearchProblemActivity.java
+++ PsRankingClient/app/src/main/java/com/project/ctp/psrankingclient/SearchProblemActivity.java
@@ -1,13 +1,10 @@
 package com.project.ctp.psrankingclient;
 
-import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.ContextMenu;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.SubMenu;
 import android.view.View;
 import android.widget.Button;
 
@@ -50,34 +47,77 @@ public class SearchProblemActivity extends ActionBarActivity {
     {
         super.onCreateContextMenu(menu, v, menuInfo);
 
-        if(v.getId()==R.id.btn_selectOldOrder) //오래된 기수를 선택했을때
+        if(v==btn_selectOldOrder) //오래된 기수를 선택했을때 group_id is 0
         {
-            MenuInflater inflater = getMenuInflater();
-            inflater.inflate(R.menu.context_order, menu);
             menu.setHeaderTitle("오래된 기수");
+            menu.add(0,0,0,"전체");
+            menu.add(0,1,0,"2011/1");
+            menu.add(0,2,0,"2011/2");
+            menu.add(0,3,0,"2012/1");
+            menu.add(0,4,0,"2012/2");
+            menu.add(0,5,0,"2013/1");
+            menu.add(0,6,0,"2013/2");
+            menu.add(0,7,0,"2014/1");
+            menu.add(0,8,0,"2014/2");
+            menu.add(0,9,0,"2015/1");
+            menu.add(0,10,0,"2015/2");
+            menu.add(0,11,0,"2016/1");
+            menu.add(0,12,0,"2016/2");
+            menu.add(0,13,0,"2017/1");
+            menu.add(0,14,0,"2017/2");
+            menu.add(0,15,0,"2018/1");
+            menu.add(0,16,0,"2018/2");
+            menu.add(0,17,0,"2019/1");
+            menu.add(0,18,0,"2019/2");
+            menu.add(0,19,0,"2020/1");
+            menu.add(0,20,0,"2020/2");
         }
-        else //최근 기수를 선택했을때
+        else if(v==btn_selectYoungOrder) //최근 기수를 선택했을때 group_id is 1
         {
-            MenuInflater inflater = getMenuInflater();
-            inflater.inflate(R.menu.context_order, menu);
             menu.setHeaderTitle("최근 기수");
+            menu.add(1, 0, 0, "전체");
+            menu.add(1, 1, 0, "2011/1");
+            menu.add(1, 2, 0, "2011/2");
+            menu.add(1, 3, 0, "2012/1");
+            menu.add(1, 4, 0, "2012/2");
+            menu.add(1, 5, 0, "2013/1");
+            menu.add(1, 6, 0, "2013/2");
+            menu.add(1, 7, 0, "2014/1");
+            menu.add(1, 8, 0, "2014/2");
+            menu.add(1, 9, 0, "2015/1");
+            menu.add(1, 10, 0, "2015/2");
+            menu.add(1, 11, 0, "2016/1");
+            menu.add(1, 12, 0, "2016/2");
+            menu.add(1, 13, 0, "2017/1");
+            menu.add(1, 14, 0, "2017/2");
+            menu.add(1, 15, 0, "2018/1");
+            menu.add(1, 16, 0, "2018/2");
+            menu.add(1, 17, 0, "2019/1");
+            menu.add(1, 18, 0, "2019/2");
+            menu.add(1, 19, 0, "2020/1");
+            menu.add(1, 20, 0, "2020/2");
         }
     }
 
     @Override
     public boolean onContextItemSelected(MenuItem item) //컨텍스트 아이템을 선택했을때(입력 확인용)
     {
-        SubMenu menu = item.getSubMenu();
-        //submenu가 없을 경우 return
-        if(menu!=null)
-            return false;
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setPositiveButton("확인", null);
-        builder.setTitle("선택된 메뉴");
-        builder.setMessage(item.getTitle());
-        builder.show();
-
+        if(item.getGroupId() == 0)
+        {
+            /*switch(item.getItemId()) 스위치문 나중에 사용 예정
+            {
+                case 1:
+            }*/
+            btn_selectOldOrder.setText(item.getTitle()); // 버튼 내용 변경
+        }
+        else if(item.getGroupId() == 1)
+        {
+            /*switch(item.getItemId()) 스위치문 나중에 사용 예정
+            {
+                case 1:
+            }*/
+            btn_selectYoungOrder.setText(item.getTitle());
+        }
         return true;
     }
 
