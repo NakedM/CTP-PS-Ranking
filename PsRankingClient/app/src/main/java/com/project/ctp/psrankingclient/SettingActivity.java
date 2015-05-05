af1e65cf2214923d1dfa2d13bc78f5f459df7663
NakedM <khmchris@naver.com>
2015-05-05 18:34:22 +0900

    SettingActivity 레이아웃 버튼, EditText 추가
    UserInfoActivity 추가
    SearchProblemActivity에 기존 오버라이드 함수 삭제
    SettingActivity에 파일 입출력 추가

--- PsRankingClient/app/src/main/java/com/project/ctp/psrankingclient/SettingActivity.java
+++ PsRankingClient/app/src/main/java/com/project/ctp/psrankingclient/SettingActivity.java
@@ -1,16 +1,78 @@
 package com.project.ctp.psrankingclient;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 
 public class SettingActivity extends ActionBarActivity {
-
+    Button btn_idSet;
+    EditText et_bojId;
+    String bojId;
+    String dn = "setting";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        btn_idSet = (Button)findViewById(R.id.btn_idSet);
+        et_bojId = (EditText)findViewById(R.id.et_bojId);
+
+        readBojId();
+
+        btn_idSet.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                storeBojId();
+            }
+        });
     }
+
+    void readBojId(){
+        try {
+            FileInputStream fis = openFileInput("userIds.txt");
+
+            byte[] buf = new byte[fis.available()];
+            fis.read(buf);
+            String str = new String(buf);
+            et_bojId.setText(str);
+            fis.close();
+        }catch (IOException e) {
+            Log.e(dn,e.getMessage());
+            e.getStackTrace().toString();
+
+        }
+
+    }
+
+    void storeBojId(){
+        bojId = et_bojId.getText().toString();
+        if(bojId.isEmpty()){
+            return;
+        }
+
+        try {
+            FileOutputStream fos = openFileOutput("userIds.txt", Context.MODE_ENABLE_WRITE_AHEAD_LOGGING);
+
+            byte[] strByte = bojId.getBytes();
+            fos.write(strByte);
+            fos.close();
+
+        }catch (IOException e) {
+            Log.e(dn,e.getMessage());
+            e.getStackTrace().toString();
+
+        }
+
+    }
+
+
 
 
 }
---
+++ PsRankingClient/app/src/main/java/com/project/ctp/psrankingclient/UserInfoActivity.java
@@ -0,0 +1,20 @@
+package com.project.ctp.psrankingclient;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class UserInfoActivity extends ActionBarActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+
+
+    }
+
+}
