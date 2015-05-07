7fc2973302b2b4c071d7d7e341c10f122c73e58f
pppgod <strangeyun@gmail.com>
2015-05-07 17:36:33 +0900

    코드정리
    메인 액티비티 변수명 정리
    컨텍스트 메뉴 생성 함수사용

--- PsRankingClient/app/src/main/java/com/project/ctp/psrankingclient/SearchProblemActivity.java
+++ PsRankingClient/app/src/main/java/com/project/ctp/psrankingclient/SearchProblemActivity.java
@@ -49,52 +49,13 @@ public class SearchProblemActivity extends ActionBarActivity {
         if(v==btn_selectOldOrder) //오래된 기수를 선택했을때 group_id is 0
         {
             menu.setHeaderTitle("오래된 기수");
-            menu.add(0,0,0,"전체");
-            menu.add(0,1,0,"2011/1");
-            menu.add(0,2,0,"2011/2");
-            menu.add(0,3,0,"2012/1");
-            menu.add(0,4,0,"2012/2");
-            menu.add(0,5,0,"2013/1");
-            menu.add(0,6,0,"2013/2");
-            menu.add(0,7,0,"2014/1");
-            menu.add(0,8,0,"2014/2");
-            menu.add(0,9,0,"2015/1");
-            menu.add(0,10,0,"2015/2");
-            menu.add(0,11,0,"2016/1");
-            menu.add(0,12,0,"2016/2");
-            menu.add(0,13,0,"2017/1");
-            menu.add(0,14,0,"2017/2");
-            menu.add(0,15,0,"2018/1");
-            menu.add(0,16,0,"2018/2");
-            menu.add(0,17,0,"2019/1");
-            menu.add(0,18,0,"2019/2");
-            menu.add(0,19,0,"2020/1");
-            menu.add(0,20,0,"2020/2");
+            setContexMenu(menu, 0);
+
         }
         else if(v==btn_selectYoungOrder) //최근 기수를 선택했을때 group_id is 1
         {
             menu.setHeaderTitle("최근 기수");
-            menu.add(1, 0, 0, "전체");
-            menu.add(1, 1, 0, "2011/1");
-            menu.add(1, 2, 0, "2011/2");
-            menu.add(1, 3, 0, "2012/1");
-            menu.add(1, 4, 0, "2012/2");
-            menu.add(1, 5, 0, "2013/1");
-            menu.add(1, 6, 0, "2013/2");
-            menu.add(1, 7, 0, "2014/1");
-            menu.add(1, 8, 0, "2014/2");
-            menu.add(1, 9, 0, "2015/1");
-            menu.add(1, 10, 0, "2015/2");
-            menu.add(1, 11, 0, "2016/1");
-            menu.add(1, 12, 0, "2016/2");
-            menu.add(1, 13, 0, "2017/1");
-            menu.add(1, 14, 0, "2017/2");
-            menu.add(1, 15, 0, "2018/1");
-            menu.add(1, 16, 0, "2018/2");
-            menu.add(1, 17, 0, "2019/1");
-            menu.add(1, 18, 0, "2019/2");
-            menu.add(1, 19, 0, "2020/1");
-            menu.add(1, 20, 0, "2020/2");
+            setContexMenu(menu, 1);
         }
     }
 
@@ -120,4 +81,28 @@ public class SearchProblemActivity extends ActionBarActivity {
         return true;
     }
 
+    public void setContexMenu(ContextMenu menu, int GID)
+    {
+        menu.add(GID, 0, 0, "전체");
+        menu.add(GID, 1, 0, "2011/1");
+        menu.add(GID, 2, 0, "2011/2");
+        menu.add(GID, 3, 0, "2012/1");
+        menu.add(GID, 4, 0, "2012/2");
+        menu.add(GID, 5, 0, "2013/1");
+        menu.add(GID, 6, 0, "2013/2");
+        menu.add(GID, 7, 0, "2014/1");
+        menu.add(GID, 8, 0, "2014/2");
+        menu.add(GID, 9, 0, "2015/1");
+        menu.add(GID, 10, 0, "2015/2");
+        menu.add(GID, 11, 0, "2016/1");
+        menu.add(GID, 12, 0, "2016/2");
+        menu.add(GID, 13, 0, "2017/1");
+        menu.add(GID, 14, 0, "2017/2");
+        menu.add(GID, 15, 0, "2018/1");
+        menu.add(GID, 16, 0, "2018/2");
+        menu.add(GID, 17, 0, "2019/1");
+        menu.add(GID, 18, 0, "2019/2");
+        menu.add(GID, 19, 0, "2020/1");
+        menu.add(GID, 20, 0, "2020/2");
+    }
 }
