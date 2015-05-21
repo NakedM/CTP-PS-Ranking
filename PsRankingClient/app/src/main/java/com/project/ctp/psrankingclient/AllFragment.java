701838cd3fb83af5e5d0e309e3fcc26e0e168820
dodo3371 <dodo3371@naver.com>
2015-05-22 01:13:44 +0900

    fragment 리스트뷰 추가
    짱깨 최고

--- PsRankingClient/app/src/main/java/com/project/ctp/psrankingclient/AllFragment.java
+++ PsRankingClient/app/src/main/java/com/project/ctp/psrankingclient/AllFragment.java
@@ -1,15 +1,55 @@
 package com.project.ctp.psrankingclient;
 
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AllFragment extends Fragment{
+    private List<String> mDataSourceList = new ArrayList<String>();
+    private List<FragmentTransaction> mBackStackList = new ArrayList<FragmentTransaction>();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_all, container, false);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //add data to ListView
+        for(int i=0, count=20; i<count; i++){
+            mDataSourceList.add("列表数据" + i);
+        }
+
+
+        ListView listView = (ListView) getActivity().findViewById(R.id.list_all);
+        listView.setAdapter(new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, mDataSourceList));
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getActivity(), UserInfoActivity.class);
+                startActivity(intent);
+                return false;
+            }
+        });
+    }
+
+    private void showTost(String msg){
+        Toast.makeText(getActivity(), msg, Toast.LENGTH_LONG).show();
     }
 }
--- PsRankingClient/app/src/main/java/com/project/ctp/psrankingclient/StudentNumberFragment.java
+++ PsRankingClient/app/src/main/java/com/project/ctp/psrankingclient/StudentNumberFragment.java
@@ -1,5 +1,7 @@
 package com.project.ctp.psrankingclient;
 
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -8,11 +10,18 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentNumberFragment extends Fragment{
     private Button btn_studentNum;
-
+    private List<String> mDataSourceList = new ArrayList<String>();
+    private List<FragmentTransaction> mBackStackList = new ArrayList<FragmentTransaction>();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_student_number, container, false);
@@ -31,6 +40,24 @@ public class StudentNumberFragment extends Fragment{
                 registerForContextMenu(v);
                 getActivity().openContextMenu(v);
                 unregisterForContextMenu(v);
+            }
+        });
+
+        //add data to ListView
+        for(int i=0, count=20; i<count; i++){
+            mDataSourceList.add("列表数据" + i);
+        }
+
+
+        ListView listView = (ListView) getActivity().findViewById(R.id.list_studentNum);
+        listView.setAdapter(new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, mDataSourceList));
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getActivity(), UserInfoActivity.class);
+                startActivity(intent);
+                return false;
             }
         });
     }
