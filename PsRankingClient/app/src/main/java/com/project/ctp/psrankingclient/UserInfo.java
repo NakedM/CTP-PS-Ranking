139245b4abd76db8411f46422fd5b5a45d98f1fe
pppgod <strangeyun@gmail.com>
2015-05-31 22:27:42 +0900

    그래프 웹뷰 크기 변경
    다른 디바이스에서도 잘 적용되는지 확인 필요함

---
+++ PsRankingClient/app/src/main/java/com/project/ctp/psrankingclient/UserInfo.java
@@ -0,0 +1,20 @@
+package com.project.ctp.psrankingclient;
+
+/**
+ * Created by Sangyun on 2015-05-27.
+ */
+public class UserInfo
+{
+    private String mem_id;
+    private String stu_id;
+    private String stu_name;
+    private String join_year;
+    private String boj_id;
+    private String total;
+    private String accept;
+    private String wrong_answer;
+    private String time_limit;
+    private String run_error;
+    private String compile_error;
+    private String etc;
+}
